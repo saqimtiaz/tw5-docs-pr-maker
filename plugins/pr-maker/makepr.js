59f025d6616ca2c16b0a47cc85d84b6364c79e32
saqimtiaz <saq.imtiaz@gmail.com>
2023-07-23 21:53:07 +0200

    feat: integrate CLA signing

--- plugins/pr-maker/makepr.js
+++ plugins/pr-maker/makepr.js
@@ -5,137 +5,138 @@ module-type: startup
 \*/
 (function(){
 
-/*jslint node: true, browser: true */
-/*global $tw: false */
-"use strict";
-
-exports.name = "makepr-handler";
-exports.platforms = ["browser"];
-exports.after = ["startup"];
-
-const REPO = "TiddlyWiki5",
-	REPO_BRANCH = "tiddlywiki-com",
-	STATUS_TITLE = "$:/state/makepr/prlink";
-
-var getTiddlerPath = function(title) {
-	var originalTiddlerPaths = JSON.parse($tw.wiki.getTiddlerText("$:/config/OriginalTiddlerPaths",{}));
-	if(originalTiddlerPaths[title]) {
-		return `editions/tw5.com/tiddlers/${originalTiddlerPaths[title]}`;
-	} else {
-		return `editions/tw5.com/tiddlers/${title}.tid`;
-	}
-}
-
-var makepr = function(files,slug,lingo) {
-	const MyOctokit = $tw.Octokit.plugin($tw.createPullRequest);
-	const TOKEN = $tw.utils.getPassword("github-docs-pr");
-	if(!TOKEN || !TOKEN.length) {
-		alert("Please set the github personal access token");
-		return;
-	}
-	const octokit = new MyOctokit({
-		auth: TOKEN,
-	});
+	/*jslint node: true, browser: true */
+	/*global $tw: false */
+	"use strict";
 	
-	// Returns a normal Octokit PR response
-	// See https://octokit.github.io/rest.js/#octokit-routes-pulls-create
-	const REPO_OWNER = $tw.wiki.getTiddlerText("$:/config/sq/makepr/repoOwner","jermolene");
-	const PR_USER_BRANCH = $tw.wiki.getTiddlerText("$:/temp/pr-user-branch",slug).substring(0,20);
-	$tw.wiki.addTiddler(new $tw.Tiddler({title: STATUS_TITLE, text: `Creating submission...`}));
-	octokit.createPullRequest({
-		owner: REPO_OWNER,
-		repo: REPO,
-		title: $tw.wiki.getTiddlerText("$:/temp/pr-title") || "Signing CLA",
-		body: $tw.wiki.getTiddlerText("$:/temp/pr-message"),
-		base: REPO_BRANCH /* To Do: allow optionally specifying branch via UI */,
-		head: `${PR_USER_BRANCH}-${Math.floor(Date.now() / 1000)}`,
-		changes: [
-			{
-			/* optional: if `files` is not passed, an empty commit is created instead */
-				files: files,
-				commit: $tw.wiki.getTiddlerText("$:/temp/pr-title") || "Signing CLA",
-				emptyCommit : false,
-			},
-		],
-		createWhenEmpty: false,
-	})
-	.then((pr) => {
-	 	//console.log(pr.data.number);
-		$tw.wiki.addTiddler(new $tw.Tiddler({title: STATUS_TITLE, text:"lingo.success", link: `https://github.com/${REPO_OWNER}/${REPO}/pull/${pr.data.number}`}));
-	}).catch((err)=>{
-		$tw.wiki.addTiddler(new $tw.Tiddler({title: STATUS_TITLE, text: `${lingo.error} ${err}`}));		  
-	});
-}
-
-var initpr = function(files,slug,lingo) {
-	if(!$tw.Octokit) {
-		$tw.wiki.addTiddler(new $tw.Tiddler({title: STATUS_TITLE, text: `loading external library`}));
-		import("https://esm.sh/@octokit/core@5.0.0").then((module)=>{
-		 $tw.Octokit = module.Octokit;
-		 return import("https://esm.sh/octokit-plugin-create-pull-request@5.1.0");
-	 	}).then((module)=>{
-			$tw.createPullRequest = module.createPullRequest;
-			makepr(files,slug,lingo);
-	 	}).catch((err)=>{
-			$tw.wiki.addTiddler(new $tw.Tiddler({title: STATUS_TITLE, text: `${lingo.error} ${err}`}));
-	 	});
-	} else {
-		makepr(files,slug,lingo);
+	exports.name = "makepr-handler";
+	exports.platforms = ["browser"];
+	exports.after = ["startup"];
+	
+	const REPO = "TiddlyWiki5",
+		REPO_BRANCH = "tiddlywiki-com",
+		STATUS_TITLE = "$:/state/makepr/prlink";
+	
+	var getTiddlerPath = function(title) {
+		var originalTiddlerPaths = JSON.parse($tw.wiki.getTiddlerText("$:/config/OriginalTiddlerPaths",{}));
+		if(originalTiddlerPaths[title]) {
+			return `editions/tw5.com/tiddlers/${originalTiddlerPaths[title]}`;
+		} else {
+			return `editions/tw5.com/tiddlers/${title}.tid`;
+		}
 	}
-}
-
-exports.startup = function() {
-	$tw.rootWidget.addEventListener("tm-makepr",function(event){
-		var filter = event.param ||"",
-			paramObject = event.paramObject || {},
-			slug = paramObject.slug,
-			tiddlersToUpload = $tw.wiki.filterTiddlers(filter,$tw.rootWidget),
-			files = {};
-		if(!paramObject.cla && tiddlersToUpload.length) {
-			$tw.utils.each(tiddlersToUpload,function(title){
-				//check if tiddler exists?
-				var tiddler = $tw.wiki.getTiddler(title);
-				if(tiddler) {
-					var path = getTiddlerPath(title),
-						wikifyParser = $tw.wiki.parseText("text/vnd.tiddlywiki",$tw.wiki.getTiddlerText("$:/core/templates/tid-tiddler"),{
-							parseAsInline: false
-						}),
-						wikifyWidgetNode = $tw.wiki.makeWidget(wikifyParser,{
-							document: $tw.fakeDocument,
-							parentWidget: $tw.rootWidget,
-							variables: {currentTiddler: title}
-						}),
-						wikifyContainer = $tw.fakeDocument.createElement("div"),
-						tid;
-					wikifyWidgetNode.render(wikifyContainer,null);
-					tid = wikifyContainer.textContent;
-					files[path] = tid;
-				}
-			});
-			var lingo = {
-				"error": "There was an error in submitting the update.",
-				"success": "Submission request created"
-			};
-			initpr(files,slug,lingo);
-		} else if(paramObject.cla) {
-			var cla = $tw.wiki.getTiddlerText(paramObject.cla,"");
-			if(cla) {
+	
+	var makepr = function(files,slug,lingo,stateTitle) {
+		const MyOctokit = $tw.Octokit.plugin($tw.createPullRequest);
+		const TOKEN = $tw.utils.getPassword("github-docs-pr");
+		if(!TOKEN || !TOKEN.length) {
+			alert("Please set the github personal access token");
+			return;
+		}
+		const octokit = new MyOctokit({
+			auth: TOKEN,
+		});
+		
+		// Returns a normal Octokit PR response
+		// See https://octokit.github.io/rest.js/#octokit-routes-pulls-create
+		const REPO_OWNER = $tw.wiki.getTiddlerText("$:/config/sq/makepr/repoOwner","jermolene");
+		const PR_USER_BRANCH = $tw.wiki.getTiddlerText("$:/temp/pr-user-branch",slug).substring(0,20);
+		$tw.wiki.addTiddler(new $tw.Tiddler({title: stateTitle, text: `Creating submission...`}));
+		octokit.createPullRequest({
+			owner: REPO_OWNER,
+			repo: REPO,
+			title: $tw.wiki.getTiddlerText("$:/temp/pr-title") || "Signing CLA",
+			body: $tw.wiki.getTiddlerText("$:/temp/pr-message"),
+			base: REPO_BRANCH /* To Do: allow optionally specifying branch via UI */,
+			head: `${PR_USER_BRANCH}-${Math.floor(Date.now() / 1000)}`,
+			changes: [
+				{
+				/* optional: if `files` is not passed, an empty commit is created instead */
+					files: files,
+					commit: $tw.wiki.getTiddlerText("$:/temp/pr-title") || "Signing CLA",
+					emptyCommit : false,
+				},
+			],
+			createWhenEmpty: false,
+		})
+		.then((pr) => {
+			 //console.log(pr.data.number);
+			$tw.wiki.addTiddler(new $tw.Tiddler({title: stateTitle, text: lingo.success, link: `https://github.com/${REPO_OWNER}/${REPO}/pull/${pr.data.number}`}));
+		}).catch((err)=>{
+			$tw.wiki.addTiddler(new $tw.Tiddler({title: stateTitle, text: `${lingo.error} ${err}`}));		  
+		});
+	}
+	
+	var initpr = function(files,slug,lingo,stateTitle) {
+		if(!$tw.Octokit) {
+			$tw.wiki.addTiddler(new $tw.Tiddler({title: stateTitle, text: `loading external library`}));
+			import("https://esm.sh/@octokit/core@5.0.0").then((module)=>{
+			 $tw.Octokit = module.Octokit;
+			 return import("https://esm.sh/octokit-plugin-create-pull-request@5.1.0");
+			 }).then((module)=>{
+				$tw.createPullRequest = module.createPullRequest;
+				makepr(files,slug,lingo,stateTitle);
+			 }).catch((err)=>{
+				$tw.wiki.addTiddler(new $tw.Tiddler({title: stateTitle, text: `${lingo.error} ${err}`}));
+			 });
+		} else {
+			makepr(files,slug,lingo,stateTitle);
+		}
+	}
+	
+	exports.startup = function() {
+		$tw.rootWidget.addEventListener("tm-makepr",function(event){
+			var filter = event.param ||"",
+				paramObject = event.paramObject || {},
+				slug = paramObject.slug,
+				stateTitle = paramObject.state,
+				tiddlersToUpload = $tw.wiki.filterTiddlers(filter,$tw.rootWidget),
+				files = {};
+			if(!paramObject.cla && tiddlersToUpload.length) {
+				$tw.utils.each(tiddlersToUpload,function(title){
+					//check if tiddler exists?
+					var tiddler = $tw.wiki.getTiddler(title);
+					if(tiddler) {
+						var path = getTiddlerPath(title),
+							wikifyParser = $tw.wiki.parseText("text/vnd.tiddlywiki",$tw.wiki.getTiddlerText("$:/core/templates/tid-tiddler"),{
+								parseAsInline: false
+							}),
+							wikifyWidgetNode = $tw.wiki.makeWidget(wikifyParser,{
+								document: $tw.fakeDocument,
+								parentWidget: $tw.rootWidget,
+								variables: {currentTiddler: title}
+							}),
+							wikifyContainer = $tw.fakeDocument.createElement("div"),
+							tid;
+						wikifyWidgetNode.render(wikifyContainer,null);
+						tid = wikifyContainer.textContent;
+						files[path] = tid;
+					}
+				});
 				var lingo = {
-					"error": "There was an error in signing the CLA.",
-					"success": "Request to sign the CLA has been submitted. You may continue."
+					"error": "There was an error in submitting the update.",
+					"success": "Submission request created"
+				};
+				initpr(files,slug,lingo,stateTitle);
+			} else if(paramObject.cla) {
+				var cla = $tw.wiki.getTiddlerText(paramObject.cla,"");
+				if(cla) {
+					var lingo = {
+						"error": "There was an error in signing the CLA.",
+						"success": "Request to sign the CLA has been submitted"
+					}
+					files["licenses/cla-individual.md"] = $tw.wiki.getTiddlerText(paramObject.cla,"");
+					initpr(files,"Signing-CLA",lingo,stateTitle);
 				}
-				files["licenses/cla-entity.md"] = $tw.wiki.getTiddlerText(paramObject.cla,"");
-				initpr(files,"Signing-CLA",lingo);
 			}
-		}
-	});
+		});
+		
+		$tw.wiki.addEventListener("change",function(changes){
+			var modifiedTiddlers = Object.keys(changes).filter(tiddler => !tiddler.startsWith("$:/") && !changes[tiddler].deleted);
+			if(modifiedTiddlers.length) {
+				$tw.rootWidget.invokeActionsByTag("$:/tags/sq/change-actions",null,{"actionTiddler": $tw.utils.stringifyList(modifiedTiddlers)});
+			}
+		});
+	};
 	
-	$tw.wiki.addEventListener("change",function(changes){
-		var modifiedTiddlers = Object.keys(changes).filter(tiddler => !tiddler.startsWith("$:/") && !changes[tiddler].deleted);
-		if(modifiedTiddlers.length) {
-			$tw.rootWidget.invokeActionsByTag("$:/tags/sq/change-actions",null,{"actionTiddler": $tw.utils.stringifyList(modifiedTiddlers)});
-		}
-	});
-};
-
-})();
+	})();
